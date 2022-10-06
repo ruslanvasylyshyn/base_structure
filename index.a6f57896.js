a97830aba7de6331449acafc01b9fc1adce7d3a7
Ruslan Vasylyshyn <r.y.vasylyshyn@gmail.com>
2022-10-07 00:34:11 +0300

    Updates

---
+++ index.a6f57896.js
@@ -0,0 +1,2 @@
+"use strict";console.log("Все ок...");
+//# sourceMappingURL=index.a6f57896.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,4 @@
+"use strict";
+console.log("Все ок...");
+
+//# sourceMappingURL=index.f75de5e1.js.map
